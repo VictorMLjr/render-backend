d6609dbbf5e7f65b8fb9106d0f0c74c481410d02
Victor <narrowblazedelta1@gmail.com>
2025-07-20 00:06:08 -0600

    More Security Made

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.post('/api/order', (req, res) => {
     const { firstN, lastN, email, phone, address, serviceWanted } = req.body;
 
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         secure: true,
         auth: {
             user: process.env.EMAIL_USER,
