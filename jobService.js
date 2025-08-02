8b5f14bb470893527057510cade15cb4e12bf8ec
Victor <narrowblazedelta1@gmail.com>
2025-08-02 12:20:20 -0600

    Added more jobs for service

--- jobService.js
+++ jobService.js
@@ -1,12 +1,27 @@
 module.exports = [
     {
         name: "Paving",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Crazy_Paving_Patio.jpg/640px-Crazy_Paving_Patio.jpg",
+        image: "",
         description: "Paving services for driveways, patios, and walkways. We use high-quality materials to ensure durability and aesthetic appeal."
     },
     {
         name: "Pluming",
-        image: "https://cdn.prod.website-files.com/643dd13153ce80ea0a9ceae9/64a2d407bfcebde9109cf23a_Bathroom%20Plumbing.png",
+        image: "",
         description: "Plumbing services for residential and commercial properties. We handle everything from leaks to installations with precision."
-    }
+    },
+    {
+        name: "Repairs",
+        image: "",
+        description: "Houeshold repair services including electrical, plumbing, and general maintenance. We ensure your home is in top condition."
+    },
+    {
+        name: "Substitute Rehab",
+        image: "",
+        description: ""
+    },
+    {
+        name: "New Construction",
+        image: "",
+        description: ""
+    },
 ];
